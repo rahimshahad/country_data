a27529e144255c75074da4c4a50da8d0bc992ea4
rahimshahad <rahim.shahad2@gmail.com>
2021-02-08 12:33:58 -0500

    refactor

--- lib/country_data/api.rb
+++ lib/country_data/api.rb
@@ -3,19 +3,19 @@ class Api
         response = HTTParty.get("https://restcountries.eu/rest/v2/all")
         response.map do |result|
          result
-         country = Country.new(name: result["name"], capital: result["capital"])
-         country.name =  result["name"]
-         country.capital = result["capital"]
-         country.population = result["population"]
-         country.gini = result["gini"]
-         country.region = result["region"]
-         country.area = result["area"]
-         country.latitude = result["latlng"][0]
-         country.longitude = result["latlng"][1]
-         country.demonym = result["demonym"]
-         country.timezone = result["timezones"]
-         country.calling_code = result["callingCodes"]
-         country.domain = result["topLevelDomain"]
+          country = Country.new(result)
+        #  country.name =  result["name"]
+        #  country.capital = result["capital"]
+        #  country.population = result["population"]
+        #  country.gini = result["gini"]
+        #  country.region = result["region"]
+        #  country.area = result["area"]
+        #  country.latitude = result["latlng"][0]
+        #  country.longitude = result["latlng"][1]
+        #  country.demonym = result["demonym"]
+        #  country.timezone = result["timezones"]
+        #  country.calling_code = result["callingCodes"]
+        #  country.domain = result["topLevelDomain"]
             end
     end
 
--- lib/country_data/country.rb
+++ lib/country_data/country.rb
@@ -1,13 +1,10 @@
 class Country
     @@all = []
     attr_accessor :name, :capital, :population, :gini, :region, :area, :latitude, :longitude, :domain, :calling_code, :timezone, :demonym
-    def initialize(name:, capital:)
-        @name = name
-        @capital = capital
-        @population = population
-        @area = area
-        @gini = gini
-        @region = region
+    def initialize(result)
+     result.each do |k,v|
+        self.send "#{k}=", v if self.respond_to?("#{k}=", v)
+     end
         save
         end
     def self.all
